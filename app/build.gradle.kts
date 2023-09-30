9f900d6575e91bb40f095e3cc2367f4b288a07b4
troemanes <mertunal1998@gmail.com>
2023-09-30 11:29:52 +0300

    retrofit,glide home meal image have done and solve problems

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,4 +64,14 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //videoModel mvvm
+    val lifecycle_version = "2.6.2"
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycle_version")
+    implementation ("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycle_version")
+   // implementation ("android.arch.lifecycle:extensions:1.1.1")
+
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-savedstate:$lifecycle_version")
+
 }
--- app/src/main/java/com/example/foodapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/foodapp/fragments/HomeFragment.kt
@@ -1,66 +1,52 @@
 package com.example.foodapp.fragments
 
+
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
-import com.example.foodapp.R
 import com.example.foodapp.databinding.FragmentHomeBinding
 import com.example.foodapp.pojo.Meal
-import com.example.foodapp.pojo.MealList
-import com.example.foodapp.retrofit.RetrofitInstance
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import com.example.foodapp.viewModel.HomeVM
 
 class HomeFragment : Fragment() {
-     private lateinit var binding:FragmentHomeBinding
-
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var homeMvvm: HomeVM
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-
-        }
+        homeMvvm = ViewModelProvider(this).get(HomeVM::class.java)
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater,container,false)
+    ): View {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
     }
 
-        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-            super.onViewCreated(view, savedInstanceState)
 
-          RetrofitInstance.api.getRandomMeal().enqueue(object:Callback<MealList>{
-              override fun onResponse(call: Call<MealList>, response: Response<MealList>) {
-                  if(response.body() != null) {
-                      val randomMeal:Meal =  response.body()!!.meals[0]
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-                 Glide.with(this@HomeFragment)
-                   .load(randomMeal.strMealThumb)
-                     .into(binding.imageRandomMeal)
-                  }else  {
-                      return
-                  }
-              }
-
-              override fun onFailure(call: Call<MealList>, t: Throwable) {
-
-                  Log.d("Home Fragment",t.message.toString())
-              }
-
-          })
-
-
+        homeMvvm.getRandomMeal()
+        observeRandomMeal()
     }
 
-
+    private fun observeRandomMeal() {
+        homeMvvm.observeRandomMealLiveData().observe(viewLifecycleOwner, object : Observer<Meal> {
+            override fun onChanged(value: Meal) {
+                Glide.with(this@HomeFragment)
+                    .load(value.strMealThumb)
+                    .into(binding.imageRandomMeal)
+            }
+        })
+    }
 }
+
 
